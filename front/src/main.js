7b68f697b1b66dbef15dd2ab4b1d5c429bbde6eb
Nicolas de Garrigues <ndegarrigues@gmail.com>
2023-01-17 12:30:47 +0100

    :wrench: UPDATE dsn config

--- front/src/main.js
+++ front/src/main.js
@@ -7,6 +7,8 @@ import {io} from 'socket.io-client'
 const app = createApp(App)
 
 // use port in .env.local file
-app.config.globalProperties.$socket = io('http://localhost:' + process.env.PORT)
+app.config.globalProperties.$socket = io(process.env.VUE_APP_SOCKET_DSN, {
+  path: '/socket.io'
+})
 
 app.use(store).use(router).mount('#app')
