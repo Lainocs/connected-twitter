6dc61b407fdad82fcc0eed4c1ade5b27bc079b71
Nicolas de Garrigues <ndegarrigues@gmail.com>
2023-01-17 18:20:23 +0100

    :wrench: UPDATE

--- back/api/router.js
+++ back/api/router.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.get('/tweets', controller.getTweets)
 router.post('/tweets', controller.createTweet)
+router.get('*', (req, res) => res.status(404).send('Il n\'y a rien ici...'))
 
 export default router
